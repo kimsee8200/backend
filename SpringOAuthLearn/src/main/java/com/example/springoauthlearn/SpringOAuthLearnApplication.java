64d8805de3ccb39b2a7040c2f8b3b831ab1a3e76
kimsee8200 <ttungseui11@gmail.com>
2024-07-07 20:27:16 +0900

    new

---
+++ SpringOAuthLearn/src/main/java/com/example/springoauthlearn/SpringOAuthLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoauthlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuthLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuthLearnApplication.class, args);
+	}
+
+}
---
+++ SpringOAuthLearn/src/test/java/com/example/springoauthlearn/SpringOAuthLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauthlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
