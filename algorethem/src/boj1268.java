4c9dfc6177dfc16c226be1c527561f0de2defd06
kimsee8200 <ttungseui11@gmail.com>
2024-08-15 18:28:02 +0900

    my study

--- algorethem/src/boj1268.java
+++ algorethem/src/boj1268.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class boj1268 {
 
@@ -16,6 +17,14 @@ public class boj1268 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[][] arr =  write(scanner);
-        
+        int[] duplicate = new int[arr.length];
+        int i;
+
+        // i*10+arr[i][j]로 값을 저장 -> 10을 나눈 값으로 정렬 -> 수가 겹침. -> stack을 이용하여 겹치면 count하면 됨.
+        for(int j = 0; j < 5; j++){
+            for (i = 0 ; i < arr.length; i++){
+                
+            }
+        }
     }
 }
--- customException/src/main/java/org/example/customexception/global/security/jwt/JwtFilter.java
+++ customException/src/main/java/org/example/customexception/global/security/jwt/JwtFilter.java
@@ -1,4 +1,20 @@
 package org.example.customexception.global.security.jwt;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        jwtUtil.
+    }
 }
